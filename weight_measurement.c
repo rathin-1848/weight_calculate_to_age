ad980ab8dfbcb85338720cbdf8648eed5dc2ebd8
rathin-1848 <rathin.baidya2026@gmail.com>
2026-08-08 07:16:12 +0530

    Add files via upload

---
+++ weight_measurement.c
@@ -0,0 +1,65 @@
+/* weight measurement */
+
+#include<stdio.h>
+
+void child(float w)
+    {
+        if (w<10)
+            printf("But Under Weight");
+        else if (w>=10 && w<=15)
+            printf("Normal Weight");
+        else
+            printf("ButOver weighted");
+    }
+    
+void young(float w)
+    {
+        if (w<50)
+            printf("But Under Weight");
+        else if (w>=50 && w<=70)
+            printf("Normal Weight");
+        else
+            printf("But Over weighted");
+    }
+
+void old(float w)
+    {
+        if (w<40)
+            printf("But Under Weight");
+        else if (w>=40 && w<=70)
+            printf("Normal Weight");
+        else
+            printf("But Over weighted");
+    }
+
+int main()
+{
+    int a;
+    float w;
+  
+ 
+
+    printf("Enter your age: ");
+    scanf("%d", &a);
+
+    printf("Enter your Body weight: ");
+    scanf("%f", &w);
+
+    if (a <= 10)
+    {
+        printf("You are a child\n");
+        child(w);
+    }
+    else if(a>10 && a<=60)
+    {
+        printf("You are young\n");
+        young(w);
+    }
+    else
+    {
+        printf("You are old\n");
+        old(w);
+    }
+
+    return 0;
+}
